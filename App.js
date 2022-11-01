d1a6d3d9bf221bdcdb7681e456d885e5911beb04
RamiPiacen <ramipiacentini1@gmail.com>
2022-10-31 23:29:30 -0300

    agrego funcionalidad al boton

--- App.js
+++ App.js
@@ -1,22 +1,62 @@
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet,Text, TextInput, View } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  FlatList,
+} from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [item, setItem] = useState("");
+  const [itemlist, setItemlist] = useState([]);
+
+  const cambiarTextoImput = (t) => {
+    setItem(t);
+  };
+
+  const presionarBoton = () => {
+    setItemlist((currenItems) => [
+      ...currenItems,
+      { id: Math.random().toString(), value: item },
+    ]);
+    console.log(item)
+    setItem("");
+  };
+  console.log(item);
+
+  const renderItem = ({item}) => (
+    <View>
+      <View style={styles.items}>
+        <Text>{item.value}</Text>
+      </View>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
       <View style={styles.flex}>
         <TextInput
+          value={item}
           placeholder="agregar al carro"
           style={styles.Imput}
           placeholderTextColor={"gray"}
+          onChangeText={cambiarTextoImput}
         />
-        <Button style={styles.boton} color="black" title="agregar" />
+        <Button
+          onPress={presionarBoton}
+          style={styles.boton}
+          color="black"
+          title="agregar"
+        />
       </View>
-      <View style={styles.textos}>
-        <Text style={styles.text}>texto1</Text>
-        <Text style={styles.text}>texto2</Text>
-        <Text style={styles.text}>texto3</Text>
-      </View>
+      <FlatList
+        data={itemlist}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
     </View>
   );
 }
@@ -36,14 +76,13 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     width: 200,
   },
-  textos:{
-  marginTop:50,
+  textos: {
+    marginTop: 50,
   },
-  text:{
-    color:"red",
-    borderWidth:1,
-    margin:10,
-    width:200,
-    
+  text: {
+    color: "red",
+    borderWidth: 1,
+    margin: 10,
+    width: 200,
   },
 });
